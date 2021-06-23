b6bb88423d054dd020e6eea9de1ef9de3af25d90
kdcllc <info@kingdavidconsulting.com>
2021-06-23 12:03:17 -0400

    linux paths

--- dotnet/sdk/build-3.1.ps1
+++ dotnet/sdk/build-3.1.ps1
@@ -1,30 +1,30 @@
 Write-Output "Building 3.1 base images";
 
-.\dotnet\sdk\build-base.ps1 -tag "3.1" -image "base"
-.\dotnet\sdk\build-base.ps1 -tag "latest" -image "base"
-.\dotnet\sdk\build-base.ps1 -tag "3.1-buster" -image "base"
-.\dotnet\sdk\build-base.ps1 -tag "3.1-bionic" -image "base"
-.\dotnet\sdk\build-base.ps1 -tag "3.1-alpine" -image "base.alpine"
+./dotnet/sdk/build-base.ps1 -tag "3.1" -image "base"
+./dotnet/sdk/build-base.ps1 -tag "latest" -image "base"
+./dotnet/sdk/build-base.ps1 -tag "3.1-buster" -image "base"
+./dotnet/sdk/build-base.ps1 -tag "3.1-bionic" -image "base"
+./dotnet/sdk/build-base.ps1 -tag "3.1-alpine" -image "base.alpine"
 
 Write-Output "Building 3.1 dev images";
 
-.\dotnet\sdk\build-base.ps1 -tag "3.1" -image "dev"
-.\dotnet\sdk\build-base.ps1 -tag "latest" -image "dev"
-.\dotnet\sdk\build-base.ps1 -tag "3.1-buster" -image "dev"
-.\dotnet\sdk\build-base.ps1 -tag "3.1-bionic" -image "dev"
-.\dotnet\sdk\build-base.ps1 -tag "3.1-alpine" -image "dev"
+./dotnet/sdk/build-base.ps1 -tag "3.1" -image "dev"
+./dotnet/sdk/build-base.ps1 -tag "latest" -image "dev"
+./dotnet/sdk/build-base.ps1 -tag "3.1-buster" -image "dev"
+./dotnet/sdk/build-base.ps1 -tag "3.1-bionic" -image "dev"
+./dotnet/sdk/build-base.ps1 -tag "3.1-alpine" -image "dev"
 
 Write-Output "Building 3.1 mini images";
 
-.\dotnet\sdk\build-base.ps1 -tag "3.1" -image "mini"
-.\dotnet\sdk\build-base.ps1 -tag "latest" -image "mini"
-.\dotnet\sdk\build-base.ps1 -tag "3.1-buster" -image "mini"
-.\dotnet\sdk\build-base.ps1 -tag "3.1-bionic" -image "mini"
-.\dotnet\sdk\build-base.ps1 -tag "3.1-alpine" -image "mini"
+./dotnet/sdk/build-base.ps1 -tag "3.1" -image "mini"
+./dotnet/sdk/build-base.ps1 -tag "latest" -image "mini"
+./dotnet/sdk/build-base.ps1 -tag "3.1-buster" -image "mini"
+./dotnet/sdk/build-base.ps1 -tag "3.1-bionic" -image "mini"
+./dotnet/sdk/build-base.ps1 -tag "3.1-alpine" -image "mini"
 
 Write-Output "Building 3.1 vscode images";
 
-.\dotnet\sdk\build-base.ps1 -tag "3.1" -image "vscode"
-.\dotnet\sdk\build-base.ps1 -tag "latest" -image "vscode"
-.\dotnet\sdk\build-base.ps1 -tag "3.1-buster" -image "vscode"
-.\dotnet\sdk\build-base.ps1 -tag "3.1-bionic" -image "vscode"
+./dotnet/sdk/build-base.ps1 -tag "3.1" -image "vscode"
+./dotnet/sdk/build-base.ps1 -tag "latest" -image "vscode"
+./dotnet/sdk/build-base.ps1 -tag "3.1-buster" -image "vscode"
+./dotnet/sdk/build-base.ps1 -tag "3.1-bionic" -image "vscode"
