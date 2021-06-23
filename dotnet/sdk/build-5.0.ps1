a08afd7e84aadd7dc898f1dd0dffe69ee4c0d63d
kdcllc <info@kingdavidconsulting.com>
2021-06-23 12:24:06 -0400

    build 5.0 and 6.0

--- dotnet/sdk/build-5.0.ps1
+++ dotnet/sdk/build-5.0.ps1
@@ -1,27 +1,27 @@
 
 Write-Output "Building 5.0 base images";
 
-.\dotnet\sdk\build-base.ps1 -tag "5.0" -image "base"
-.\dotnet\sdk\build-base.ps1 -tag "5.0-buster-slim" -image "base"
-.\dotnet\sdk\build-base.ps1 -tag "5.0-focal" -image "base"
-.\dotnet\sdk\build-base.ps1 -tag "5.0-alpine" -image "base.alpine"
+./dotnet/sdk/build-base.ps1 -tag "5.0" -image "base"
+./dotnet/sdk/build-base.ps1 -tag "5.0-buster-slim" -image "base"
+./dotnet/sdk/build-base.ps1 -tag "5.0-focal" -image "base"
+./dotnet/sdk/build-base.ps1 -tag "5.0-alpine" -image "base.alpine"
 
 Write-Output "Building 5.0 dev images";
 
-.\dotnet\sdk\build-base.ps1 -tag "5.0" -image "dev"
-.\dotnet\sdk\build-base.ps1 -tag "5.0-buster-slim" -image "dev"
-.\dotnet\sdk\build-base.ps1 -tag "5.0-focal" -image "dev"
-.\dotnet\sdk\build-base.ps1 -tag "5.0-alpine" -image "dev"
+./dotnet/sdk/build-base.ps1 -tag "5.0" -image "dev"
+./dotnet/sdk/build-base.ps1 -tag "5.0-buster-slim" -image "dev"
+./dotnet/sdk/build-base.ps1 -tag "5.0-focal" -image "dev"
+./dotnet/sdk/build-base.ps1 -tag "5.0-alpine" -image "dev"
 
 Write-Output "Building 5.0 mini images";
 
-.\dotnet\sdk\build-base.ps1 -tag "5.0" -image "mini"
-.\dotnet\sdk\build-base.ps1 -tag "5.0-buster-slim" -image "mini"
-.\dotnet\sdk\build-base.ps1 -tag "5.0-focal" -image "mini"
-.\dotnet\sdk\build-base.ps1 -tag "5.0-alpine" -image "mini"
+./dotnet/sdk/build-base.ps1 -tag "5.0" -image "mini"
+./dotnet/sdk/build-base.ps1 -tag "5.0-buster-slim" -image "mini"
+./dotnet/sdk/build-base.ps1 -tag "5.0-focal" -image "mini"
+./dotnet/sdk/build-base.ps1 -tag "5.0-alpine" -image "mini"
 
 Write-Output "Building 5.0 vscode images";
 
-.\dotnet\sdk\build-base.ps1 -tag "5.0" -image "vscode"
-.\dotnet\sdk\build-base.ps1 -tag "5.0-buster-slim" -image "vscode"
-.\dotnet\sdk\build-base.ps1 -tag "5.0-focal" -image "vscode"
+./dotnet/sdk/build-base.ps1 -tag "5.0" -image "vscode"
+./dotnet/sdk/build-base.ps1 -tag "5.0-buster-slim" -image "vscode"
+./dotnet/sdk/build-base.ps1 -tag "5.0-focal" -image "vscode"
--- dotnet/sdk/build-6.0.ps1
+++ dotnet/sdk/build-6.0.ps1
@@ -1,30 +1,30 @@
 
 Write-Output "Building 6.0 base images";
 
-.\dotnet\sdk\build-base.ps1 -tag "6.0" -image "base"
-.\dotnet\sdk\build-base.ps1 -tag "6.0-bullseye-slim" -image "base"
-.\dotnet\sdk\build-base.ps1 -tag "6.0-focal" -image "base"
-.\dotnet\sdk\build-base.ps1 -tag "6.0-alpine" -image "base.alpine"
-.\dotnet\sdk\build-base.ps1 -tag "6.0-alpine3.13" -image "base.alpine"
+./dotnet/sdk/build-base.ps1 -tag "6.0" -image "base"
+./dotnet/sdk/build-base.ps1 -tag "6.0-bullseye-slim" -image "base"
+./dotnet/sdk/build-base.ps1 -tag "6.0-focal" -image "base"
+./dotnet/sdk/build-base.ps1 -tag "6.0-alpine" -image "base.alpine"
+./dotnet/sdk/build-base.ps1 -tag "6.0-alpine3.13" -image "base.alpine"
 
 Write-Output "Building 6.0 dev images";
 
-.\dotnet\sdk\build-base.ps1 -tag "6.0" -image "dev"
-.\dotnet\sdk\build-base.ps1 -tag "6.0-bullseye-slim" -image "dev"
-.\dotnet\sdk\build-base.ps1 -tag "6.0-focal" -image "dev"
-.\dotnet\sdk\build-base.ps1 -tag "6.0-alpine" -image "dev"
-.\dotnet\sdk\build-base.ps1 -tag "6.0-alpine3.13" -image "dev"
+./dotnet/sdk/build-base.ps1 -tag "6.0" -image "dev"
+./dotnet/sdk/build-base.ps1 -tag "6.0-bullseye-slim" -image "dev"
+./dotnet/sdk/build-base.ps1 -tag "6.0-focal" -image "dev"
+./dotnet/sdk/build-base.ps1 -tag "6.0-alpine" -image "dev"
+./dotnet/sdk/build-base.ps1 -tag "6.0-alpine3.13" -image "dev"
 
 Write-Output "Building 6.0 mini images";
 
-.\dotnet\sdk\build-base.ps1 -tag "6.0" -image "mini"
-.\dotnet\sdk\build-base.ps1 -tag "6.0-bullseye-slim" -image "mini"
-.\dotnet\sdk\build-base.ps1 -tag "6.0-focal" -image "mini"
-.\dotnet\sdk\build-base.ps1 -tag "6.0-alpine" -image "mini"
-.\dotnet\sdk\build-base.ps1 -tag "6.0-alpine3.13" -image "mini"
+./dotnet/sdk/build-base.ps1 -tag "6.0" -image "mini"
+./dotnet/sdk/build-base.ps1 -tag "6.0-bullseye-slim" -image "mini"
+./dotnet/sdk/build-base.ps1 -tag "6.0-focal" -image "mini"
+./dotnet/sdk/build-base.ps1 -tag "6.0-alpine" -image "mini"
+./dotnet/sdk/build-base.ps1 -tag "6.0-alpine3.13" -image "mini"
 
 Write-Output "Building 6.0 vscode images";
 
-.\dotnet\sdk\build-base.ps1 -tag "6.0" -image "vscode"
-.\dotnet\sdk\build-base.ps1 -tag "6.0-bullseye-slim" -image "vscode"
-.\dotnet\sdk\build-base.ps1 -tag "6.0-focal" -image "vscode"
+./dotnet/sdk/build-base.ps1 -tag "6.0" -image "vscode"
+./dotnet/sdk/build-base.ps1 -tag "6.0-bullseye-slim" -image "vscode"
+./dotnet/sdk/build-base.ps1 -tag "6.0-focal" -image "vscode"
